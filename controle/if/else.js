94fab4df1e10a73d5922bc8b9a257ac31f6fbe3c
Pedro Menegussi <pedrovipmenegussi@gmail.com>
2021-02-19 16:54:04 -0300

    news adds

---
+++ controle/if/else.js
@@ -0,0 +1,10 @@
+function soBoaNoticia(nota){
+    if(nota>=7){
+        console.log("Aprovado com nota: " + nota);
+    }else{
+        console.log("Reprovado com nota: " + nota)
+    }
+}
+
+soBoaNoticia(7)
+soBoaNoticia(5)
